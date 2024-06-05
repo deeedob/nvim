ac6e8ca411b73ddb3f5ea46bac2d67dad9a3d253
deeedob <dennis.ob@protonmail.com>
2024-06-05 18:13:51 +0200

    remove nvim-ufo

--- lua/ddob/plugins/ui.lua
+++ lua/ddob/plugins/ui.lua
@@ -12,27 +12,6 @@ return {
   },
 
   {
-    "kevinhwang91/nvim-ufo",
-    dependencies = "kevinhwang91/promise-async",
-    config = function()
-      vim.o.foldcolumn = "1"
-      vim.o.foldlevel = 99
-      vim.o.foldlevelstart = 99
-      vim.o.foldenable = true
-      vim.o.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
-
-      vim.keymap.set("n", "zR", require("ufo").openAllFolds, { desc = "" })
-      vim.keymap.set("n", "zM", require("ufo").closeAllFolds, { desc = "" })
-
-      require("ufo").setup {
-        provider_selector = function(bufnr, filetype, buftype)
-          return { "treesitter", "indent" }
-        end,
-      }
-    end,
-  },
-
-  {
     "b0o/incline.nvim",
     event = "VeryLazy",
     config = function()
--- plugin/options.lua
+++ plugin/options.lua
@@ -30,6 +30,12 @@ opt.relativenumber = false
 opt.splitbelow = true
 opt.splitright = true
 
+vim.o.foldcolumn = "1"
+vim.o.foldlevel = 99
+vim.o.foldlevelstart = 99
+vim.o.foldenable = true
+vim.o.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
+
 opt.shada = { "'10", "<0", "s10", "h" }
 
 opt.wrap = false
